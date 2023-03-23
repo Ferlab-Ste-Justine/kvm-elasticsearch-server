054cd1f4a30b71aaf8883d451356c9c40a765f95
Sébastien Bonami <sbonami@ferlab.bio>
2023-03-23 17:25:44 -0400

    feat: INFRA-295 set version of each provider to keep stability

--- versions.tf
+++ versions.tf
@@ -4,6 +4,14 @@ terraform {
       source = "dmacvicar/libvirt"
       version = "= 0.6.11"
     }
+    template = {
+      source  = "hashicorp/template"
+      version = "= 2.2.0"
+    }
+    tls = {
+      source  = "hashicorp/tls"
+      version = "= 3.4.0"
+    }
   }
   required_version = ">= 1.0.0"
 }
